9924afd5eb91b9c2add887b449c20941c5d3c48e
Ben Peters <bspe227@uky.edu>
2020-05-25 05:23:58 -0500

    Created score class

--- app/src/main/java/com/example/tetrisrecords/addViewActivity.java
+++ app/src/main/java/com/example/tetrisrecords/addViewActivity.java
@@ -3,7 +3,11 @@ package com.example.tetrisrecords;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class addViewActivity extends AppCompatActivity {
 
@@ -13,6 +17,16 @@ public class addViewActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_add_view);
+        Button submitBtn = findViewById(R.id.submitBtn);
+        final EditText scoreEntry = findViewById(R.id.scoreEntry);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GameStat game = new GameStat();
+                game.setScore(Integer.parseInt(scoreEntry.getText().toString()));
+                scoreEntry.setText("");
 
+            }
+        });
     }
 }
