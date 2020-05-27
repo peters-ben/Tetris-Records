4e62437334cec592e9e5ec697a75ad2ef258a795
Ben Peters <bspe227@uky.edu>
2020-05-26 20:04:33 -0500

    Starting level can now be entered, fixed bug where not entering a score or level would crash app

--- app/src/main/java/com/example/tetrisrecords/addViewActivity.java
+++ app/src/main/java/com/example/tetrisrecords/addViewActivity.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -28,23 +30,33 @@ public class addViewActivity extends AppCompatActivity {
         final ArrayList<TextView> scoreDisplays = new ArrayList<>();
         final EditText scoreEntry = findViewById(R.id.scoreEntry);
         final EditText dateEntry = findViewById(R.id.dateEntry);
+        final EditText levelEntry = findViewById(R.id.levelEntry);
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 GameStat newGame = new GameStat();
-                newGame.setScore(Integer.parseInt(scoreEntry.getText().toString()));
-                newGame.setDate(dateEntry.getText().toString());
-                games.add(newGame);
-                scoreEntry.setText("");
-                dateEntry.setText("");
-                final TextView newTextView = new TextView(getApplicationContext());
-                newTextView.setText(String.format(getResources().getString(R.string.display),
-                        games.get(games.size() - 1).getDate(), games.get(games.size() - 1).getScore()));
-                newTextView.setTextColor(Color.WHITE);
-                newTextView.setTextSize(40);
-                newTextView.setGravity(Gravity.CENTER);
-                linearLayout.addView(newTextView);
-                scoreDisplays.add(newTextView);
+                if(!scoreEntry.getText().toString().equals("")) {
+                    newGame.setScore(Integer.parseInt(scoreEntry.getText().toString()));
+                    newGame.setDate(dateEntry.getText().toString());
+                    if(levelEntry.getText().toString().equals(""))
+                        newGame.setLevel(0);
+                    else
+                        newGame.setLevel(Integer.parseInt(levelEntry.getText().toString()));
+                    games.add(newGame);
+                    scoreEntry.setText("");
+                    levelEntry.setText("");
+                    dateEntry.setText("");
+                    final TextView newTextView = new TextView(getApplicationContext());
+                    newTextView.setText(String.format(getResources().getString(R.string.display),
+                            games.get(games.size() - 1).getDate(), games.get(games.size() - 1).getScore(),
+                            games.get(games.size() - 1).getLevel()));
+                    newTextView.setTextColor(Color.WHITE);
+                    newTextView.setTextSize(40);
+                    newTextView.setGravity(Gravity.CENTER);
+                    linearLayout.addView(newTextView);
+                    scoreDisplays.add(newTextView);
+
+                }
             }
         });
     }
